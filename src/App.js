84cf1d35de1e47b4766453f46dd7ac6616aa4505
DESKTOP-IKO3LOH\Admin <truonggnguyen0868795985@gmail.com>
2021-04-05 13:51:59 +0700

    pagination

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import queryString from "query-string";
+import Pagination from "./components/Pagination";
+import PostList from "./components/PostList";
+import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
+import PostFiltersForm from "./components/PostFiltersForm";
 
 function App() {
   const [todoList, setTodoList] = useState([
@@ -8,6 +13,42 @@ function App() {
     { id: 2, title: "I love easy front end 2" },
     { id: 3, title: "I love easy front end 3" },
   ]);
+
+  const [postList, setPostList] = useState([]);
+  const [pagination, setPagination] = useState({
+    _page: 1,
+    _limit: 10,
+    _totalRows: 11,
+  });
+  const [filters, setFilters] = useState({
+    _limit: 10,
+    _page: 1,
+    title_like: "",
+  });
+
+  useEffect(() => {
+    async function fecthPostList() {
+      try {
+        const paramsString = queryString.stringify(filters);
+        const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+
+        const { data, pagination } = responseJSON;
+        setPostList(data);
+        setPagination(pagination);
+      } catch (error) {
+        console.log("Fail to fech data from post list:", error.message);
+      }
+    }
+
+    fecthPostList();
+  }, [filters]);
+
+  function handlePageChange(newPage) {
+    console.log("New Page: ", newPage);
+    setFilters({ ...filters, _page: newPage });
+  }
 
   function handleTodoClick(todo) {
     console.log(todo);
@@ -20,11 +61,39 @@ function App() {
     setTodoList(newTodoList);
   }
 
+  function handleTodoFormSubmit(formValues) {
+    console.log(formValues);
+
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    };
+
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+
+    setTodoList(newTodoList);
+  }
+
+  function handleFiltersChange(newFilters) {
+    console.log("New filters: ", newFilters);
+    setFilters({
+      ...filters,
+      _page: 1,
+      title_like: newFilters.searchTerm,
+    });
+  }
+
   return (
     <div className="app">
-      <h1>React hook - todo list</h1>
+      <h1>React hook - Post list</h1>
 
-      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+      {/* <TodoForm onSubmit={handleTodoFormSubmit} /> */}
+      {/* <TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
+
+      <PostFiltersForm onSubmit={handleFiltersChange} />
+      <PostList posts={postList} />
+      <Pagination pagination={pagination} onPageChange={handlePageChange} />
     </div>
   );
 }
